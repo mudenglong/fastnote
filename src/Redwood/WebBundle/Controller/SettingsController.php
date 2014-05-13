4955a1134ffdc9b3be1eab9b088b07797262d309
somePlace <mudenglong@163.com>
2014-05-13 17:27:27 +0800

    upload photo step1

--- src/Redwood/WebBundle/Controller/SettingsController.php
+++ src/Redwood/WebBundle/Controller/SettingsController.php
@@ -2,6 +2,8 @@
 
 namespace Redwood\WebBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Redwood\Common\FileToolkit;
 class SettingsController extends BaseController
 {
 
@@ -13,13 +15,25 @@ class SettingsController extends BaseController
         ));
     }
 
-    public function avatarAction()
+    public function avatarAction(Request $request)
     {
         $user = $this->getCurrentUser();
         $form = $this->createFormBuilder()
             ->add('avatar', 'file')
             ->getForm();
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $file = $data['avatar'];
+                
+                if (!FileToolkit::isImageFile($file)) {
+                    return $this->createMessageResponse('error', '上传图片格式错误，请上传jpg, gif, png格式的文件。');
+                }
+            }
 
+        }
+        
         return $this->render('RedwoodWebBundle:Settings:avatar.html.twig',array(
             'user' => $user,
             'form' => $form->createView(),
