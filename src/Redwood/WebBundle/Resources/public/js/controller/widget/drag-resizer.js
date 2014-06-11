5a3ea0289e8adb2decb5423f028408ff1a73a06d
somePlace <mudenglong@163.com>
2014-06-11 14:31:49 +0800

    crop html temp: resize 8 div

--- src/Redwood/WebBundle/Resources/public/js/controller/widget/drag-resizer.js
+++ src/Redwood/WebBundle/Resources/public/js/controller/widget/drag-resizer.js
@@ -4,13 +4,14 @@ define(function(require, exports, module) {
     require("../../../css/html-crop.css");
 
     /*rectangle obj */
-    function Box(state) {
-        this.state = state;
+    function Box(DragResizer) {
+        this.dragResizerObj = DragResizer;
+        this.state = this.dragResizerObj.options;
         this.x = 0;
         this.y = 0;
         this.w = 50; 
         this.h = 30;
-        this.fillColor = '#000000';
+        this.fillColor = 'rgba(0,255,0,.7)';
     }
 
     Box.prototype.contains = function(mx, my) {
@@ -23,8 +24,10 @@ define(function(require, exports, module) {
     Box.prototype.drawBox = function(ctx) {
         ctx.fillStyle = this.fillColor;
         ctx.fillRect(this.x, this.y, this.w, this.h);
-console.log(this.state.selection);
-         if (this.state.selection === this) {
+        // console.log(this.state.selection);
+        // if (this.state.selection === this) {
+            console.log("111111");
+            console.log(this.state.selectionColor);
             ctx.strokeStyle = this.state.selectionColor;
             ctx.lineWidth = this.state.selectionWidth;
             ctx.strokeRect(this.x,this.y,this.w,this.h);
@@ -71,7 +74,7 @@ console.log(this.state.selection);
                 var cur = this.state.selectionHandles[i];
                 ctx.fillRect(cur.x, cur.y, this.state.selectionBoxSize, this.state.selectionBoxSize);
             }
-        }
+        // }
     };
 
   
@@ -92,7 +95,6 @@ console.log(this.state.selection);
             canvasID: null,
             canvas:null,
 
-            
             boxes: [],
 
             _canvasHeight: null,
@@ -106,14 +108,18 @@ console.log(this.state.selection);
             beDragging: false,
             selection: null,
 
+            beResizeDragging: false,
+            //return which selectionHandles when dragging selectionHandles
+            expectResize: -1,
+
+        },
+        options:{
             //八个角上的拖动手柄
             selectionHandles : [],
             selectionColor: '#CC0000',
             selectionBoxColor: 'darkred',
             selectionBoxSize: 6,
             selectionWidth: 2,
-
-
 
         },
         myDraw: null,
@@ -122,7 +128,8 @@ console.log(this.state.selection);
         setup: function()
         {
             var i, 
-                selectionHandles = this.get('selectionHandles');
+                selectionHandles = this.options.selectionHandles;
+
 
 
             for (i = 0; i < 8; i += 1) {
@@ -135,8 +142,11 @@ console.log(this.state.selection);
             this.initCanvas(this.get('canvasID'));
 
             // add rectangle
-            this.addRectangle(this, 200, 200, 40, 40, '#aaff4e');
+            this.addRectangle(this, 200, 200, 40, 40, 'rgba(0,255,0,.7)');
             // this.addRectangle(25, 90, 25, 25, '#2BB8FF');
+
+
+           
 
             //@todo 把时间refresh的操作显示出来
             this.draw();
@@ -146,6 +156,7 @@ console.log(this.state.selection);
         //Initialize a new Box, add it, and invalidate the canvas
         addRectangle:function (that, x, y, w, h, fillColor) 
         {
+            console.log(that);
             var rectangle = new Box(that);
             rectangle.x = x;
             rectangle.y = y;
@@ -213,7 +224,7 @@ console.log(this.state.selection);
                     obj.valid = true;
                 }
                 // console.log('valid' + obj.valid);
-            }, 30);
+            }, 1000);
         },
 
 
@@ -244,9 +255,6 @@ console.log(this.state.selection);
 
             for (var i = l-1; i >= 0; i--) {
                 if (boxes[i].contains(mouseX, mouseY)) {
-                    console.log("000000000000");
-
-
                     selectedBox = boxes[i];
                     var dragOffsetX = mouseX - selectedBox.x;
                     this.set('dragOffsetX', dragOffsetX); 
@@ -273,9 +281,10 @@ console.log(this.state.selection);
 
         },
         myMouseMove: function(e){
+
             if (this.get('beDragging')) {
             
-                mouse = this.getMouse(e);
+                var mouse = this.getMouse(e);
                 // We don't want to drag the object by its top-left corner, we want to drag it
                 // from where we clicked. Thats why we saved the offset and use it here
                 this.get('selection').x = mouse.x - this.get('dragOffsetX');
@@ -283,11 +292,128 @@ console.log(this.state.selection);
                 // Something is dragging so we must redraw
                 this.valid = false;
 
+                // return;
+            }else if (this.get('beResizeDragging')) {
+              // time ro resize!
+              oldx = this.get('selection').x;
+              oldy = this.get('selection').y;
+              console.log('enter!');
+              // 0  1  2
+              // 3     4
+              // 5  6  7
+              // switch (myState.expectResize) {
+              //   case 0:
+              //     myState.selection.x = mx;
+              //     myState.selection.y = my;
+              //     myState.selection.w += oldx - mx;
+              //     myState.selection.h += oldy - my;
+              //     break;
+              //   case 1:
+              //     myState.selection.y = my;
+              //     myState.selection.h += oldy - my;
+              //     break;
+              //   case 2:
+              //     myState.selection.y = my;
+              //     myState.selection.w = mx - oldx;
+              //     myState.selection.h += oldy - my;
+              //     break;
+              //   case 3:
+              //     myState.selection.x = mx;
+              //     myState.selection.w += oldx - mx;
+              //     break;
+              //   case 4:
+              //     myState.selection.w = mx - oldx;
+              //     break;
+              //   case 5:
+              //     myState.selection.x = mx;
+              //     myState.selection.w += oldx - mx;
+              //     myState.selection.h = my - oldy;
+              //     break;
+              //   case 6:
+              //     myState.selection.h = my - oldy;
+              //     break;
+              //   case 7:
+              //     myState.selection.w = mx - oldx;
+              //     myState.selection.h = my - oldy;
+              //     break;
+              // }
+              // this.valid = false;
+            }
+
+            var mouse2 = this.getMouse(e);
+            var mx = mouse2.x;
+            var my = mouse2.y;
+
+            var selectionHandles = this.options.selectionHandles;
+            var selectionBoxSize = this.options.selectionBoxSize;
+            var that = e.currentTarget;
+
+            // if there's a selection see if we grabbed one of the selection handles
+            if (this.get('selection') !== null && !this.get('beResizeDragging')) {
+         
+                for (var i = 0; i < 8; i += 1) {
+                    // 0  1  2
+                    // 3     4
+                    // 5  6  7
+                    cur = selectionHandles[i];
 
 
 
-                return;
-            }; 
+                    // we dont need to use the ghost context because
+                    // 确定鼠标悬停的是哪个托拽框
+                    if (mx >= cur.x && mx <= cur.x + selectionBoxSize &&
+                    my >= cur.y && my <= cur.y + selectionBoxSize) {
+
+                        // we found one!
+                        this.set('expectResize', i);
+
+                        this.valid = false;
+              
+
+                        switch (i) {
+                          case 0:
+                            that.style.cursor='nw-resize';
+                            break;
+                          case 1:
+                            that.style.cursor='n-resize';
+                            break;
+                          case 2:
+                            that.style.cursor='ne-resize';
+                            break;
+                          case 3:
+                            that.style.cursor='w-resize';
+                            break;
+                          case 4:
+                            that.style.cursor='e-resize';
+                            break;
+                          case 5:
+                            that.style.cursor='sw-resize';
+                            break;
+                          case 6:
+                            that.style.cursor='s-resize';
+                            break;
+                          case 7:
+                            that.style.cursor='se-resize';
+                            break;
+                        }
+                        return;
+                    }
+
+                }
+                // not over a selection box, return to normal
+                this.set('beResizeDragging', false);
+                this.set('expectResize', -1);
+
+                // this.style.cursor = 'auto';
+            }
+
+
+
+
+
+
+
+
         },
 
         myMouseUp: function(e)
@@ -297,7 +423,7 @@ console.log(this.state.selection);
         myDbclick: function(e)
         {
             var mouse = this.getMouse(e);
-            this.addRectangle((mouse.x - 20), (mouse.y - 20), 80, 40, 'rgba(0,255,0,.7)');
+            this.addRectangle(this, (mouse.x - 20), (mouse.y - 20), 80, 40, 'rgba(0,255,0,.7)');
             this.valid = false;
         },
 
